17181e3d326128db089f9224b51b3c43c175085a
Ireme-Nyawe <akimana.inono@gmail.com>
2025-02-21 12:47:27 +0200

    c++ assignment one- run notes expamle

---
+++ OOP/Assignment-1/ExampleProgram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age = 20;  // Integer variable
+    double price = 19.99;  // Double variable
+    char grade = 'A';  // Character variable
+    string name = "Alice";  // String variable
+
+    cout << "Name: " << name << endl;
+
+    
+    return 0;
+}
